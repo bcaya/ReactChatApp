aaa697b745a26b3e500156362901b04d348156fa
Bobby Caya <bob.caya1@gmail.com>
2018-04-10 11:29:33 -0600

    Add chat feature

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,6 +11,9 @@ class NavBar extends Component {
     if (user.id) {
       return (
         <Menu.Menu position='right'>
+        <Link to="/chat">
+          <Menu.Item>Chat</Menu.Item>
+        </Link>
           <Menu.Item
             name='Logout'
             onClick={() => dispatch(handleLogout(history))}
