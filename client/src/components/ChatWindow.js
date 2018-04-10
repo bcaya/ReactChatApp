f4606e7272c5e30b65eb0175f13f2b604a4a8e44
Bobby Caya <bob.caya1@gmail.com>
2018-04-10 10:20:37 -0600

    initial commit

---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { 
+  Segment, 
+  Header, 
+  Form, 
+  TextArea, 
+  Button 
+} from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { setFlash } from '../actions/flash';
+import { addMessage } from '../actions/messages';
+import ChatMessage from './ChatMessage';
+
+class ChatWindow extends React.Component {
+  render() {
+    return null
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    messages: state.messages,
+  }
+}
+
+export default connect(mapStateToProps)(ChatWindow);
